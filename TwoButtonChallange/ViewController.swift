e9b076a9eaa6229e661107f2af9ed1f071bf35ea
Bruce Wheeler <bruce.wheeler18@alumni.colostate.edu>
2023-10-23 16:02:23 -0600

    Alignment text exercise

--- TwoButtonChallange/ViewController.swift
+++ TwoButtonChallange/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
+    
+    }
+    @IBAction func messageButtonPressed2(_ sender: UIButton) {
+        messageLabel.text = "You Are Great"
+        messageLabel.textColor = UIColor.systemGreen
+        messageLabel.textAlignment = .right
+    }
 }
 
