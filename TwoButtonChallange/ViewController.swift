b8a916540837c42cf9e008621d0a9b88729206bf
Bruce Wheeler <bruce.wheeler18@alumni.colostate.edu>
2023-10-23 19:41:42 -0600

    Message button pressed text was added

--- TwoButtonChallange/ViewController.swift
+++ TwoButtonChallange/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("The messsage button was pressed")
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
